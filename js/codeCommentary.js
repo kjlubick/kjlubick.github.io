44e05aba310205b034051aa12ad08ab818da4933
Kevin Lubick <kjlubick@ncsu.edu>
2014-04-29 22:16:21 -0400

    Line two gets highlighted!

--- js/codeCommentary.js
+++ js/codeCommentary.js
@@ -1,5 +1,35 @@
-$(document).ready(new function() {
+function highlightLines(pre, lines, classes) {
+	var ranges = lines.replace(/\s+/g, '').split(','),
+	    offset = +pre.getAttribute('data-line-offset') || 0;
 	
-	console.log("codeCommentary.js");
+	var lineHeight = parseFloat(getComputedStyle(pre).lineHeight);
 
+	for (var i=0, range; range = ranges[i++];) {
+		range = range.split('-');
+					
+		var start = +range[0],
+		    end = +range[1] || start;
+		
+		var line = $(pre).find(".line-highlight");
+		line.show();
+
+/*    //if the line-numbers plugin is enabled, then there is no reason for this plugin to display the line numbers
+    if(!$(pre).hasClass("line-numbers")) {
+      line.data("start",start);
+
+      if(end > start) {
+        line.data("end", end);
+      }*/
+    }
+
+		line.css("top", (start - offset - 1) * lineHeight + 'px');
+}
+
+
+
+$(document).ready(function() {
+	console.log("codeCommentary.js");
+	$(".codeContainer").on("mouseenter","pre", function(e) {
+		highlightLines(e.currentTarget, "2");
+	});
 });
