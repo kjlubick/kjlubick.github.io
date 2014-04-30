5a4c4d06fc099077176d9d91786c50a9f28ddf0f
Kevin Lubick <kjlubick@ncsu.edu>
2014-04-29 22:32:36 -0400

    More JSHint

--- js/codeCommentary.js
+++ js/codeCommentary.js
@@ -1,35 +1,25 @@
 function highlightLines(pre, lines, classes) {
-	var ranges = lines.replace(/\s+/g, '').split(','),
-	    offset = +pre.getAttribute('data-line-offset') || 0;
-	
-	var lineHeight = parseFloat(getComputedStyle(pre).lineHeight);
+    var ranges = lines.replace(/\s+/g, '').split(','),
+        offset = +pre.getAttribute('data-line-offset') || 0,
+        lineHeight = parseFloat(getComputedStyle(pre).lineHeight), i, start, line, range;
 
-	for (var i=0, range; range = ranges[i++];) {
-		range = range.split('-');
-					
-		var start = +range[0],
-		    end = +range[1] || start;
-		
-		var line = $(pre).find(".line-highlight");
-		line.show();
+    for (i = 0; range = ranges[i++];) {
+        range = range.split('-');
 
-/*    //if the line-numbers plugin is enabled, then there is no reason for this plugin to display the line numbers
-    if(!$(pre).hasClass("line-numbers")) {
-      line.data("start",start);
+        start = +range[0];
 
-      if(end > start) {
-        line.data("end", end);
-      }*/
+        line = $(pre).find(".line-highlight");
+        line.show();
     }
 
-		line.css("top", (start - offset - 1) * lineHeight + 'px');
+    line.css("top", (start - offset - 1) * lineHeight + 'px');
 }
 
 
 
-$(document).ready(function() {
-	console.log("codeCommentary.js");
-	$(".codeContainer").on("mouseenter","pre", function(e) {
-		highlightLines(e.currentTarget, "2");
-	});
+$(document).ready(function () {
+    console.log("codeCommentary.js");
+    $(".codeContainer").on("mouseenter", "pre", function (e) {
+        highlightLines(e.currentTarget, "2");
+    });
 });
