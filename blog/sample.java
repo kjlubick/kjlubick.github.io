cdc9cc1ab3bf439cdec763149efec4c6a465e0f0
Kevin Lubick <kjlubick@ncsu.edu>
2014-05-10 18:33:56 -0400

    Made scrollable code a thing
    Now you can limit the code container's height and scrolling will work.
    I recommend you don't go any shorter than 450px.
    
    Also some css tweaks to the code containing pre, which will prevent the
    scroll bar from randomly appearing.

--- blog/sample.java
+++ blog/sample.java
@@ -1,6 +1,68 @@
-	System.out.println("Hello World");
-	if (this != null) {
-		System.exit(0);
-		System.out.println("Hello World, This is an incredibly long line of code that stretches a very long distance");
-		System.exit(0);
+package com.mebigfatguy.fbcontrib.detect;
+
+import org.apache.bcel.classfile.Code;
+import org.apache.bcel.classfile.Method;
+
+import com.mebigfatguy.fbcontrib.debug.Debug;
+
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.BytecodeScanningDetector;
+import edu.umd.cs.findbugs.ba.ClassContext;
+
+public class MoreInfiniteLoops extends BytecodeScanningDetector {
+
+	private final BugReporter bugReporter;
+	
+	public MoreInfiniteLoops(BugReporter reporter) {
+		this.bugReporter=reporter;
 	}
+
+	@Override
+	public void visitClassContext(ClassContext classContext) {
+		Debug.println("Visiting ClassContext "+classContext);
+		super.visitClassContext(classContext);
+	}
+	
+	@Override
+	public void visitMethod(Method obj) {
+		Debug.println("Visiting Method "+obj);
+		super.visitMethod(obj);
+	}
+	
+	@Override
+	public void visitCode(Code obj) {
+		Debug.println("Visiting code "+obj);
+		super.visitCode(obj);
+	}
+	
+	@Override
+	public void sawOpcode(int seen) {
+		Debug.println("Saw opcode");
+		super.sawOpcode(seen);
+	}
+	
+		@Override
+	public void visitClassContext(ClassContext classContext) {
+		Debug.println("Visiting ClassContext "+classContext);
+		super.visitClassContext(classContext);
+	}
+	
+	@Override
+	public void visitMethod(Method obj) {
+		Debug.println("Visiting Method "+obj);
+		super.visitMethod(obj);
+	}
+	
+	@Override
+	public void visitCode(Code obj) {
+		Debug.println("Visiting code "+obj);
+		super.visitCode(obj);
+	}
+	
+	@Override
+	public void sawOpcode(int seen) {
+		Debug.println("Saw opcode");
+		super.sawOpcode(seen);
+	}
+	
+}
