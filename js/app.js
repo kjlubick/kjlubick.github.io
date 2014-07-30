67a5d58fdd2cf69cf20e5666049dc8466d5099c1
Kevin Lubick <kjlubick@ncsu.edu>
2014-07-30 18:01:17 -0400

    Set up more blog template
    Incorporated moment.js and ng-relative, but now the times seem to be
    fixed...

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
+  'timeRelative',
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
